1f5cc86f5f5010831337e22120c1fe2d2a76b87b
candyTong <563378816@qq.com>
2019-02-23 17:16:41 +0800

    修改：统一app.js 启动代码

--- src/app.js
+++ src/app.js
@@ -3,17 +3,26 @@ import regeneratorRuntime from './utils/third-party/runtime'    // eslint-disabl
 import { config } from './config/config'
 import { constant } from './config/constant'
 import { initSocket } from './utils/lib/scoket'
+
 const Raven = require('./utils/third-party/raven')
 
 App({
     onLaunch: async function () {
         console.log(`environment ${config.environment} release: ${config.release}`)
-        if (config.environment === constant.environment && config.sentry) {
-            Raven.config(config.sentry.dsn, config.sentry.options).install()
-        }
+
+        // init sentry
+        this.initSentry()
 
         // init Socket.io
         initSocket()
+    },
+    // 初始化sentry
+    initSentry() {
+        if (config.environment === constant.environment && config.sentry) {
+            Raven.config(config.sentry.dsn, config.sentry.options).install()
+        } else {
+            console.log('开发环境不初始化sentry')
+        }
     },
     globalData: {
         oauthSession: {},   // 资源服务器session
